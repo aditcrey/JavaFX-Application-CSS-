c8b8cd943399a9440abc5b3ac67cdaf2d9ccdf19
Aditya Thakre <aditcrey@gmail.com>
2018-01-05 23:07:51 +0530

    dropShadow effect for button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 
 public class Controller {
     @FXML
     private Label label;
 
+    @FXML
+    private Button button4;
+
 
     public void initialize(){
+        button4.setEffect(new DropShadow());
 
     }
 
@@ -24,3 +30,8 @@ public class Controller {
         label.setScaleY(1.0);
     }
 }
+
+
+/**
+ link for all the effects in javaFX : https://docs.oracle.com/javase/8/javafx/api/javafx/scene/effect/package-frame.html
+ **/
