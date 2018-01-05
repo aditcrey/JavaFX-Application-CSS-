39763a3109d4cda8e3a7418e4988a39e1102bf87
Aditya Thakre <aditcrey@gmail.com>
2018-01-05 23:20:13 +0530

    fileChooser

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 public class Controller {
     @FXML
@@ -11,6 +13,9 @@ public class Controller {
 
     @FXML
     private Button button4;
+
+    @FXML
+    private GridPane gridPane;
 
 
     public void initialize(){
@@ -29,9 +34,26 @@ public class Controller {
         label.setScaleX(1.0);
         label.setScaleY(1.0);
     }
+
+    @FXML
+    public void handleClick(){
+        FileChooser chooser = new FileChooser();
+        //we are gonna call the show open dialog method which shows the filechooser in open mode but to do that
+        //we need to pass in a parent window for the filechoose...so we need to assign fx:id to our GridPane
+//        chooser.showOpenDialog(null); //in this case you'll be able to interact with the application window even though the open dialog is open //not wanted
+        chooser.showOpenDialog(gridPane.getScene().getWindow());
+    }
 }
 
 
 /**
  link for all the effects in javaFX : https://docs.oracle.com/javase/8/javafx/api/javafx/scene/effect/package-frame.html
+ **/
+
+
+/**
+ * we can the open and save feature for our application using file chooser class...but it doesn't descend from the node class
+ * and hence can't be added to the scene graph...in other words, we can't add to layouts  ...but there is no need
+ * the file chooser ui does it by itself and we don't have to worry about embedding it with the layout
+ *
  **/
