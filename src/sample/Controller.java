3fa7990777dd7ae2d29571190a70c34aa750ba5e
Aditya Thakre <aditcrey@gmail.com>
2018-01-05 23:25:03 +0530

    DirectoryChooser

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
     @FXML
@@ -37,11 +40,25 @@ public class Controller {
 
     @FXML
     public void handleClick(){
+        /*
         FileChooser chooser = new FileChooser();
         //we are gonna call the show open dialog method which shows the filechooser in open mode but to do that
         //we need to pass in a parent window for the filechoose...so we need to assign fx:id to our GridPane
 //        chooser.showOpenDialog(null); //in this case you'll be able to interact with the application window even though the open dialog is open //not wanted
         chooser.showOpenDialog(gridPane.getScene().getWindow());
+
+
+        */
+
+        //using DirectoryChooser to be able to select a folder(directory)
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.showDialog(gridPane.getScene().getWindow()); //here we need to use showDialog instead of showOpenDialog
+        
+
+
+
+
+
     }
 }
 
@@ -55,5 +72,8 @@ public class Controller {
  * we can the open and save feature for our application using file chooser class...but it doesn't descend from the node class
  * and hence can't be added to the scene graph...in other words, we can't add to layouts  ...but there is no need
  * the file chooser ui does it by itself and we don't have to worry about embedding it with the layout
+ *
+ *
+ * if we want to choose a folder(directory) instead of a file then we need to use directoryChooser instead of fileChooser
  *
  **/
